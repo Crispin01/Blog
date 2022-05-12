d76fee8aba6329941a48272d48f71298514dad9b
unknown <sergiocrispin07@gmail.com>
2022-05-11 21:54:30 -0500

    casi vuelo

--- single.php
+++ single.php
@@ -1,13 +1,16 @@
 <?php get_header();?>
 		<section>
 			<div class="container">
-				<h4><?php the_title(); ?></h4>
+				<h6><?php the_field('resumen');?></h6>
 				
 					<center><div class="imagen">
-		 			<?php the_post_thumbnail("thumbnail"); ?>
+		 			<?php the_post_thumbnail("largue"); ?>
 					</div></center>
+				<h4><?php the_title(); ?></h4>
+
 				<?php the_content(); ?>
 			
+				<h6>Fuente: <?php the_field('fuente');?></h6>
 			</div>
 
 		</section>
